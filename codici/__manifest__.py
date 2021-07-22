5fe973f4d078c2f34baf70c4c17537dcd3eac11d
dattolo <francesco.dattolo@gmail.com>
2021-07-22 15:12:03 +0200

    Changes to manifest

--- codici/__manifest__.py
+++ codici/__manifest__.py
@@ -5,11 +5,12 @@
     'summary': "Francesco Dattolo Codici"
     
     """
+        Pure Test Step 
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        Pure Test Step Long description of module's purpose
     """,
 
     'author': "Francesco Dattolo",
@@ -18,15 +19,15 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'drricambi',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product'],
 
     # always loaded
     'data': [
-        'data/francesco_codici_data.xml',
+        #'data/francesco_codici_data.xml',
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
