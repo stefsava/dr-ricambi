46c3198e18cf5e29086e906026ade8bf38bd1d16
dattolo <francesco.dattolo@gmail.com>
2021-07-22 15:28:13 +0200

    Changes to manifest

--- codici/__manifest__.py
+++ codici/__manifest__.py
@@ -20,7 +20,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'drricambi',
-    'version': '0.2',
+    'version': '0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'product'],
