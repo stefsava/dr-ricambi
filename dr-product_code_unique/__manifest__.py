83042de2f046c336c090ac76aa9ca85cdac4b3ef
dattolo <info@francescodattolo.it>
2021-08-02 17:13:42 +0200

    Unique Product Internal Reference and Product Brand

--- dr-product_code_unique/__manifest__.py
+++ dr-product_code_unique/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Customization of the 'Unique Product Internal Reference' OCA module",
+    "name": "'Unique Product Internal Reference and Product Brand' drricambi module",
     "summary": "Set Product Internal Reference and Product brand  with as Unique",
     "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
-    "category": "Product",
+    "category": "Product, drricambi",
     "website": "https://github.com/OCA/product-attribute",
     "depends": ["product"],
     "pre_init_hook": "pre_init_product_code",
